e5807f5b6630b51dcabc9589f0569588f88527ff
Karl <karl.wasson@gmail.com>
2023-08-30 21:50:53 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 require('dotenv').config({ path: './.env' });
 const express = require('express');
 const app = express();
@@ -5,9 +6,22 @@ const outlineRoute = require('./routes/outline');
 const errorHandler = require('./middleware/error');
 const corsHandler = require('./middleware/cors');
 
+// Existing middleware
 app.use(express.json());
 app.use(corsHandler);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Anything that doesn't match the above, send back the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+});
+
+// Your existing API routes
 app.use('/', outlineRoute);
+
+// Error handling
 app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
