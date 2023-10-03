52901b3c7f84af15ac1ca25fa70c1ab3c50ab1fe
Karl <karl.wasson@gmail.com>
2023-10-03 20:10:20 +0700

    update routes

--- backend/routes/outline.js
+++ backend/routes/outline.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const outlineController = require('../controllers/outlineController');
 
-router.post('/generateOutline', outlineController.generateOutline);
+router.post('/api/generateOutline', outlineController.generateOutline);
 
 module.exports = router;
